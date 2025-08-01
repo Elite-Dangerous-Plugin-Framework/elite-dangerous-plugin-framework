b4ed82ce032c8f4438c2ec5b6fe7cb07bd55ab58
CMDR-WDX <83889860+CMDR-WDX@users.noreply.github.com>
2025-08-01 02:09:20 +0200

    wip: add event loop, still needs batching

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,10 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+    tracing_subscriber::fmt()
+        .with_file(true)
+        .with_line_number(true)
+        .pretty()
+        .init();
     elite_dangerous_plugin_framework_lib::run()
 }
