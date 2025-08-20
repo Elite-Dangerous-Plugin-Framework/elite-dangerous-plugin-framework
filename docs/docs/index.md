a99f5191901b1a08050da30176fdcfaadbb8bc4d
CMDR-WDX <83889860+CMDR-WDX@users.noreply.github.com>
2025-08-20 22:49:35 +0200

    docs: deploy docs

--- docs/docs/index.md
+++ docs/docs/index.md
@@ -4,11 +4,14 @@ pageType: home
 hero:
   name: E:D Plugin Framework
   text:
-  tagline: Enhance your gameplay with 3rd party plugins with ease.
+  tagline: Enhance your E:D experience with 3rd party plugins with ease.
   actions:
     - theme: brand
       text: Quick Start
       link: /guide/
+    - theme: alt
+      text: Quick Start for Devs
+      link: /guide-dev/
     - theme: alt
       text: GitHub
       link: https://github.com/CMDR-WDX/elite-dangerous-plugin-framework
--- docs/rspress.config.ts
+++ docs/rspress.config.ts
@@ -1,7 +1,16 @@
 import * as path from "node:path";
 import { defineConfig } from "rspress/config";
-
+import { pluginIconify } from "rsbuild-plugin-iconify";
 export default defineConfig({
+  plugins: [
+    pluginIconify({
+      targetDir: "src/styles/icons", // Directory to save generated CSS
+      includeSets: ["mdi-light", "material-symbols"], // Icon sets to include completely
+      maxIconsPerSet: 200, // Max icons per included set
+      maxTotalIcons: 1000, // Max total icons
+      compress: true, // Apply compression
+    }),
+  ],
   root: path.join(__dirname, "docs"),
   title: "Elite: Dangerous Plugin Framework",
   icon: "/rspress-icon.png",
